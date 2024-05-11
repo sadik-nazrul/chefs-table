75b0ce0abb693349a28c527f47863f01e6f36440
Yoursadik-nazrul <sadikull3@gmail.com>
2024-05-11 22:26:16 +0600

    Basic setup Header Creation, Fake data creation and load done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import NavbarComponent from "./components/Header/NavbarComponent";
+import Main from "./components/Main/Main";
+
+const App = () => {
+  return (
+    <div className="container mx-auto">
+      <NavbarComponent />
+      <Main />
+    </div>
+  )
+}
+export default App;
